8ef3efd1ff459ac21e85ad129fa4fe87af28d6ed
MarcusPy827 <markus_verify@126.com>
2025-08-20 01:06:28 +0800

    [Root] Configured: Dark mode of antd

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,26 +1,46 @@
-import { JSX } from "react"
-import { Splitter } from "antd"
+import { JSX, useState, useEffect, useCallback } from "react"
+import {ConfigProvider, Splitter, theme} from "antd"
 import TitleBar from "./components/TitleBar"
 import ToolBar from "./components/ToolBar"
 import MyLibrary from "./pages/MyLibrary"
 import "./assets/main.css"
 
 export default function App(): JSX.Element {
+  const [darkMode, setDarkMode] = useState(false)
+  const darkModeQuery = window.matchMedia("(prefers-color-scheme: dark)")
+
+  const onDarkModeChanged = useCallback((event: MediaQueryListEvent) => {
+    setDarkMode(event.matches)
+  }, [])
+
+  useEffect(() => {
+    darkModeQuery.addEventListener('change', onDarkModeChanged)
+    return () => {
+      darkModeQuery.removeEventListener('change', onDarkModeChanged)
+    }
+  }, [darkModeQuery, darkModeQuery])
+
+  useEffect(() => {
+    setDarkMode(darkModeQuery.matches)
+  }, []);
+
   return (
     <>
-      <TitleBar />
-      <ToolBar />
-      <Splitter className="main-panel-container">
-        <Splitter.Panel className="side-panel" defaultSize="30%" min="25%" max="30%">
-          <div className="side-panel-content">
-            [Work in progress...]
-          </div>
-        </Splitter.Panel>
+      <ConfigProvider theme={{ algorithm: darkMode ? theme.darkAlgorithm : theme.compactAlgorithm }}>
+        <TitleBar />
+        <ToolBar />
+        <Splitter className="main-panel-container">
+          <Splitter.Panel className="side-panel" defaultSize="30%" min="25%" max="30%">
+            <div className="side-panel-content">
+              [Work in progress...]
+            </div>
+          </Splitter.Panel>
 
-        <Splitter.Panel className="content-panel">
-          <MyLibrary />
-        </Splitter.Panel>
-      </Splitter>
+          <Splitter.Panel className="content-panel">
+            <MyLibrary />
+          </Splitter.Panel>
+        </Splitter>
+      </ConfigProvider>
     </>
   )
 }
