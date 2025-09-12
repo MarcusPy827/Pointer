cadc1e0ab50d94cf57e63ce7bf46ad9d059792c6
MarcusPy827 <markus_verify@126.com>
2025-09-12 17:15:21 -0500

    [Frontend] [Menu bar] Added: Menu bar layout

--- src/renderer/src/components/TitleBar.tsx
+++ src/renderer/src/components/TitleBar.tsx
@@ -3,22 +3,39 @@ import { getAppVersionString } from './Versions'
 import { WindowCommandType } from '../../../shared/WindowCommandType'
 import 'mdui/dist/css/mdui.min.css'
 import 'mdui/dist/js/mdui.min.js'
+import type { MenuProps } from 'antd';
+import { Dropdown } from 'antd'
 
 export default function TitleBar(): JSX.Element {
   const onWindowResize = async (type: WindowCommandType): Promise<void> => {
     // @ts-ignore API field already defined in preload
     await window.api.resizeWindowFunc(type)
   }
+
+  const fileMenuItems: MenuProps['items'] = [
+    {
+      key: '1',
+      label: <span>Create workspace</span>,
+      icon: null
+    },
+    {
+      key: '2',
+      label: <span>Open workspace</span>,
+      icon: null
+    }
+  ]
+
   return (
     <>
       <div className="title-bar no-select">
         <div className="title-bar-section no-select">
-          <span className="app-title no-select" draggable={false}>
-            Pointer
-          </span>
-          <span className="app-version no-select" draggable={false}>
-            REL {getAppVersionString()}
-          </span>
+          <Dropdown menu={{ items: fileMenuItems }}>
+            <span className="titlebar-menu">File</span>
+          </Dropdown>
+        </div>
+        <div className="title-bar-section drag-but-no-select">
+          <span className="app-title drag-but-no-select">Pointer</span>
+          <span className="app-version drag-but-no-select">REL {getAppVersionString()}</span>
         </div>
 
         <div className="title-bar-btn-section no-select">
